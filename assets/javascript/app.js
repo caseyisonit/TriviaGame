537b6cb2bda1ba9da959f69a70f826a06cca38ad
casey is on it <caseyeickhoffdesign@gmail.com>
2019-05-14 22:40:30 -0600

    applied a double layer background and psuedo coded the javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//===Global Variables===/
+// object array for the questions, answersArray (options), correct answer, image/gif
+//var correctcounter = correct++;
+//var wrongcounter = wrong++; 
+
+
+//===Function===/
+//startgame
+//question timer
+// answer timer
+// for loop to run through the array 
+//onclick event for single selection
+    //if/then
+//empty/reset function
+
+//===Callbacks===/
