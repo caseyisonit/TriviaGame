6fe1578c7e4fe21c49169afa1998fbdb89322db9
casey is on it <caseyeickhoffdesign@gmail.com>
2019-05-17 22:58:25 -0600

    started on the JS. got the guestion to print to the HTML. working on the answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,64 @@
 //===Global Variables===/
 // object array for the questions, answersArray (options), correct answer, image/gif
-//var correctcounter = correct++;
-//var wrongcounter = wrong++; 
+var questionArray = [
+    {
+        question: "Which queen is famous for the catchphrase 'absolutely.'",
+        answers: [
+            { answer: 'Gia Gunn', value: true },
+            { answer: 'Vivacious', value: false },
+            { answer: 'Jade Jolie', value: false },
+            { answer: 'Adore Delano', value: false }
+        ],
+        image: "https://media.giphy.com/media/26gs7ff1HFaA7KOUE/source.gif"
+    },
+    {
+        question: "Which queen is famous for the catchphrase 'absolutely.'",
+        answers: [
+            { answer: 'Gia Gunn', value: true },
+            { answer: 'Vivacious', value: false },
+            { answer: 'Jade Jolie', value: false },
+            { answer: 'Adore Delano', value: false }
+        ],
+    }
+];
+// console.log(questionArray[0]);
+var correctcounter = correct++;
+var wrongcounter = wrong++;
+var correct = 0;
+var wrong = 0;
 
+var timer = $("#timer");
+var questionDiv = $("#question");
+var answersDiv = $("#answers");
+var buttonText = $(".button")
 
 //===Function===/
-//startgame
-//question timer
-// answer timer
-// for loop to run through the array 
-//onclick event for single selection
+$(document).ready(function () {
+    //Global functions
+    //Function to print question and answers
+    function createQuestion() {
+        for (var i = 0; i < questionArray.length; i++) {
+            $(questionDiv).text(questionArray[i].question);
+
+            //for loop to print the answers
+            for (var j = 0; j < questionArray[i].answers.length; j++){
+                $(answersDiv).html("<div class='button'>" + questionArray[i].answers[j].answer + "</div>");
+
+                console.log("this is: " + $(this))
+            } 
+        };
+    };
+    createQuestion(questionArray);
+
+    //startgame
+    //question timer
+    // answer timer
+    // for loop to run through the array 
+    //onclick event for single selection
     //if/then
-//empty/reset function
+    //empty/reset function
+
+});
 
 //===Callbacks===/
+
