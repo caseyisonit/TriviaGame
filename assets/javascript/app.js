61fe3f76143c33ddf5bc99bf538075492dc54672
casey is on it <caseyeickhoffdesign@gmail.com>
2019-05-18 11:26:43 -0600

    got the time working with the create question function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,8 +26,10 @@ var correctcounter = correct++;
 var wrongcounter = wrong++;
 var correct = 0;
 var wrong = 0;
+var timer = 15;
+var intervalId;
 
-var timer = $("#timer");
+var timerDiv = $("#timer");
 var questionDiv = $("#question");
 var answersDiv = $("#answers");
 var buttonText = $(".button");
@@ -39,22 +41,50 @@ $(document).ready(function () {
     //Global functions
     //Function to print question and answers
     function createQuestion() {
+// for loop to print question and answers
         for (var i = 0; i < questionArray.length; i++) {
             var question = questionArray[i].question;
-            $(questionDiv).text(question);
+            questionDiv.text(question);
 
-            $(answersDiv).empty();
+            answersDiv.empty();
 
             function createAnswers() {
                 for (var k = 0; k < questionArray[i].answers.length; k++) {
                     var answers = questionArray[i].answers[k];
-                    $(answersDiv).append("<div class='button'>" + answers.answer + "</div>");
+                    answersDiv.append("<div class='button'>" + answers.answer + "</div>");
                 };
             };
             createAnswers();
         };
+
+//timer functions
+        function countdown() {
+            timerDiv.text(timer);
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        };
+
+        function decrement() {
+            timer--;
+            timerDiv.text(timer);
+            if (timer === 0) {
+                stop();
+                alert("Time Up!");
+            }
+        };
+
+        function stop() {
+            clearInterval(intervalId);
+        };
+
+        countdown();
+//end time functions
+
     };
     createQuestion();
+
+
+    //function for incorrect answer diplay
 
     //startgame
     //question timer
